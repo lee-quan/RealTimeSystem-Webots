ec88b95c26efbda0b310242f3897e7c59abe8a79
lee-quan <leequan2000@outlook.com>
2023-01-20 01:21:08 +0800

    sorting

--- controllers/my_controller/my_controller.py
+++ controllers/my_controller/my_controller.py
@@ -13,9 +13,14 @@ camera.recognitionEnable(timestep)
 counter=0
 i=0
 status = 'WAITING'
-targetRed=[-1.0,2.0,-1.0,0.0]
-targetBlue=[-0.532,0.05,0]
-targetGreen=[0.443,0.05,0]
+targetRed=[-1.88,-2.14,-2.3,0]
+targetBlue=[-1.88,2.14,-2,-1]
+targetGreen=[-3,-0.5,-2.3,0.3]
+test=[-3,-0.5,-2.3,0.3]
+
+# targetRed=[-1.88,-2.14]
+# targetBlue=[-1.88,2.14]
+# targetGreen=[-1.88,-1.14]
 
 handmotor=[]
 handmotor.append(robot.getDevice('finger_1_joint_1'))
@@ -27,14 +32,15 @@ urmotor.append(robot.getDevice('shoulder_lift_joint'))
 urmotor.append(robot.getDevice('elbow_joint'))
 urmotor.append(robot.getDevice('wrist_1_joint'))
 urmotor.append(robot.getDevice('wrist_2_joint'))
+shoulderpanjoint = robot.getDevice('shoulder_pan_joint')
+wrist3joint = robot.getDevice('wrist_3_joint')
 
-# urmotor[2].setPosition(0)
-# urmotor[0].setPosition(0)
-# urmotor[1].setPosition(0)
+# urmotor[0].setPosition(test[0])
+# urmotor[1].setPosition(test[1])
+# urmotor[2].setPosition(test[2])
+# urmotor[3].setPosition(test[3])
 
-# urmotor[3].setPosition(-1)
-
-speed = 1.0;
+speed = 3.0;
 for x in urmotor:
     x.setVelocity(speed)
 distanceSensor = robot.getDevice('distance sensor')
@@ -56,10 +62,10 @@ while robot.step(timestep) != -1:
             c=i.getColors()[3*j+2]
 
     if counter <=0:
-
+        
         match status:
             case 'WAITING':
-                if(distanceSensor.getValue() < 200):
+                if(distanceSensor.getValue() < 500):
                     counter=8
                     print(counter, "COUNTER!!")
                     status='GRABBING'
@@ -67,21 +73,38 @@ while robot.step(timestep) != -1:
                     for i in handmotor:
                         i.setPosition(0.85)
                         
-                        
             case 'GRABBING':
-                counter=50
+                counter=30
                 status='ROTATING'
+                if(a==1.0):
+                    target=targetRed
+                elif(b==1.0):
+                    target=targetGreen
+                else:
+                    target=targetBlue
+                print('ROTATING')
                 for i in range(4):
-                    urmotor[i].setPosition(targetRed[i])
+                    urmotor[i].setPosition(target[i])
                     
             case 'ROTATING':
+                status='RELEASING'
+                print(status)
                 counter=8
                 for i in handmotor:
                     i.setPosition(i.getMinPosition())
-
+            case 'RELEASING':
+                status='ROTATING BACK'
+                print(status);
+                for i in urmotor:
+                    i.setPosition(0.0)
+                shoulderpanjoint.setPosition(0.0)
+                wrist3joint.setPosition(0.0)
+            case 'ROTATING BACK':
+                if(positionSensor.getValue() > -0.1):
+                    status='WAITING'
+                    print(status)
 
     else:
-
         counter = counter -1
     pass
 
